8435ec6326547119ce1ee7dc2569e5208215abb9
LORY Arthur <alory@zaka-services.com>
2023-02-19 14:22:36 +0100

    create activity "infos" and "Produits"

---
+++ app/src/main/java/com/example/projet_android/InformationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projet_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_information)
+    }
+}
--- app/src/main/java/com/example/projet_android/MainActivity.kt
+++ app/src/main/java/com/example/projet_android/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.projet_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +12,16 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
 
         setHeaderTitle(getString(R.string.first_menu))
+
+        val buttonLogin=findViewById<Button>(R.id.buttonInfo)
+        buttonLogin.setOnClickListener(View.OnClickListener {
+            val newIntent = Intent(application, InformationActivity::class.java)
+            startActivity(newIntent)
+        })
+        val buttonSignup=findViewById<Button>(R.id.buttonProduits)
+        buttonSignup.setOnClickListener(View.OnClickListener {
+            val newIntent = Intent(application, ProduitActivity::class.java)
+            startActivity(newIntent)
+        })
     }
 }
